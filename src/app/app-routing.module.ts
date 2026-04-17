a902c321c2b72cc842cb15141f330e41d35e049b
Ranim <ranim.larbi@isimg.tn>
2026-04-17 14:14:34 +0100

    workshop v3 routage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './core/home/home.component';
+import { ListSuggestionComponent } from './features/suggestions/list-suggestion/list-suggestion.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // route par défaut
+  { path: 'home', component: HomeComponent },
+  { path: 'listSuggestion', component: ListSuggestionComponent },
+  { path: 'suggestions', loadChildren: () => import('./features/suggestions/suggestions.module').then(m => m.SuggestionsModule) },
+  { path: 'users', loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule) },
+  { path: '**', component: NotfoundComponent } // route inconnue
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
